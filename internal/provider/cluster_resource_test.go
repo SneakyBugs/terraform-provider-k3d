a791052c66c4e550a319cc2e75cc2d6cfa57fdf9
Lior Kummer <lior@sneakybugs.com>
2022-11-24 02:43:26 +0200

    Add test for cluster credentials feature

--- internal/provider/cluster_resource_test.go
+++ internal/provider/cluster_resource_test.go
@@ -1,14 +1,67 @@
 package provider
 
 import (
-	"fmt"
 	"os/exec"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
-const k3dConfig = `apiVersion: k3d.io/v1alpha4
+func TestAccClusterResource(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			// Create and Read testing
+			{
+				Config: testAccClusterResourceConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("scaffolding_cluster.test", "name", "k3d-provider-test"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "id"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "host"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "client_certificate"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "client_key"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "cluster_ca_certificate"),
+				),
+			},
+			// Read and recreate if missing testing
+			{
+				PreConfig: func() {
+					// Delete the cluster created in the previous step.
+					cmd := exec.Command("k3d", "cluster", "delete", "k3d-provider-test")
+					err := cmd.Run()
+					if err != nil {
+						t.Error(err)
+					}
+				},
+				Config: testAccClusterResourceConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("scaffolding_cluster.test", "name", "k3d-provider-test"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "id"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "host"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "client_certificate"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "client_key"),
+					resource.TestCheckResourceAttrSet("scaffolding_cluster.test", "cluster_ca_certificate"),
+				),
+			},
+			// Authentication to Kubernetes provider testing
+			{
+				Config: testAccClusterUsageConfig(),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrSet("kubernetes_config_map.test", "generation"),
+				),
+			},
+			// Delete testing automatically occurs in TestCase
+		},
+	})
+}
+
+func testAccClusterResourceConfig() string {
+	return `
+resource "scaffolding_cluster" "test" {
+	name = "k3d-provider-test"
+  k3d_config = <<EOF
+apiVersion: k3d.io/v1alpha4
 kind: Simple
 
 # Expose ports 80 via 8080 and 443 via 8443.
@@ -24,49 +77,50 @@ registries:
   create:
     name: dev
     hostPort: "5000"
+EOF
+}
 `
-
-func TestAccClusterResource(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:                 func() { testAccPreCheck(t) },
-		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
-		Steps: []resource.TestStep{
-			// Create and Read testing
-			{
-				Config: testAccClusterResourceConfig(k3dConfig),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("scaffolding_cluster.test", "k3d_config", k3dConfig),
-					resource.TestCheckResourceAttr("scaffolding_cluster.test", "name", "k3d-provider-test"),
-					resource.TestCheckResourceAttr("scaffolding_cluster.test", "id", "42f6391d7823ace7eb7d2d71ea5fb771"),
-				),
-			},
-			// Read and recreate if missing testing
-			{
-				PreConfig: func() {
-					// Delete the cluster created in the previous step.
-					cmd := exec.Command("k3d", "cluster", "delete", "k3d-provider-test")
-					err := cmd.Run()
-					if err != nil {
-						t.Error(err)
-					}
-				},
-				Config: testAccClusterResourceConfig(k3dConfig),
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttr("scaffolding_cluster.test", "k3d_config", k3dConfig),
-					resource.TestCheckResourceAttr("scaffolding_cluster.test", "name", "k3d-provider-test"),
-					resource.TestCheckResourceAttr("scaffolding_cluster.test", "id", "42f6391d7823ace7eb7d2d71ea5fb771"),
-				),
-			},
-			// Delete testing automatically occurs in TestCase
-		},
-	})
 }
 
-func testAccClusterResourceConfig(k3dConfig string) string {
-	return fmt.Sprintf(`
+func testAccClusterUsageConfig() string {
+	return `
 resource "scaffolding_cluster" "test" {
 	name = "k3d-provider-test"
-  k3d_config = %[1]q
+  k3d_config = <<EOF
+apiVersion: k3d.io/v1alpha4
+kind: Simple
+
+# Expose ports 80 via 8080 and 443 via 8443.
+ports:
+  - port: 3080:80
+    nodeFilters:
+      - loadbalancer
+  - port: 3443:443
+    nodeFilters:
+      - loadbalancer
+
+registries:
+  create:
+    name: dev
+    hostPort: "5000"
+EOF
 }
-`, k3dConfig)
+
+provider "kubernetes" {
+	host = resource.scaffolding_cluster.test.host
+	client_certificate = base64decode(resource.scaffolding_cluster.test.client_certificate)
+	client_key = base64decode(resource.scaffolding_cluster.test.client_key)
+	cluster_ca_certificate = base64decode(resource.scaffolding_cluster.test.cluster_ca_certificate)
+}
+
+resource "kubernetes_config_map" "test" {
+	metadata {
+		name = "test"
+	}
+
+	data = {
+		test = "acceptance"
+	}
+}
+`
 }
